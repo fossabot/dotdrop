76b7f031ad0311f13644919478e368a5ed8739fe
dotiful <artdevjs@gmail.com>
2019-07-26 00:44:15 +0300

    update

--- dotfiles/config/zsh/config/keybindings.zsh
+++ dotfiles/config/zsh/config/keybindings.zsh
@@ -46,23 +46,23 @@ bindkey '^x^x' edit-command-line
 # Automatically expanding zsh global aliases
 # https://goo.gl/fJbtmJ
 #
-globalias() {
-  zle _expand_alias
-  zle expand-word
-  zle self-insert
-}
-zle -N globalias
+# globalias() {
+#   zle _expand_alias
+#   zle expand-word
+#   zle self-insert
+# }
+# zle -N globalias
 
-# space expands all aliases, including global
-bindkey -M emacs " " globalias
-bindkey -M viins " " globalias
+# # space expands all aliases, including global
+# bindkey -M emacs " " globalias
+# bindkey -M viins " " globalias
 
-# control-space to make a normal space
-bindkey -M emacs "^ " magic-space
-bindkey -M viins "^ " magic-space
+# # control-space to make a normal space
+# bindkey -M emacs "^ " magic-space
+# bindkey -M viins "^ " magic-space
 
-# normal space during searches
-bindkey -M isearch " " magic-space
+# # normal space during searches
+# bindkey -M isearch " " magic-space
 
 
 
